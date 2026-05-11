2e3621e67225227c1cdd02a5e58126f2f8342c96
Gyan <gyan0304@gmail.com>
2026-05-10 21:55:41 -0400

    added exmaple code snippets for  pytorch and tensorflow/keras

--- src/components/CodeImportPanel.jsx
+++ src/components/CodeImportPanel.jsx
@@ -88,7 +88,85 @@ function WarningCard({ warnings }) {
     </div>
   )
 }
-
+const EXAMPLES = [
+    {
+      label: 'VGG-style CNN',
+      fw: 'pytorch',
+      code: `import torch
+  import torch.nn as nn
+  
+  class VGGBlock(nn.Module):
+      def __init__(self):
+          super().__init__()
+          self.conv1 = nn.Conv2d(3, 64, kernel_size=3, padding=1)
+          self.conv2 = nn.Conv2d(64, 64, kernel_size=3, padding=1)
+          self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2)
+          self.conv3 = nn.Conv2d(64, 128, kernel_size=3, padding=1)
+          self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2)
+          self.flatten = nn.Flatten()
+          self.fc1 = nn.Linear(128, 512)
+          self.drop = nn.Dropout(p=0.5)
+          self.fc2 = nn.Linear(512, 10)
+  
+      def forward(self, x):
+          x = self.conv1(x)
+          x = self.conv2(x)
+          x = self.pool1(x)
+          x = self.conv3(x)
+          x = self.pool2(x)
+          x = self.flatten(x)
+          x = self.fc1(x)
+          x = self.drop(x)
+          x = self.fc2(x)
+          return x`,
+    },
+    {
+      label: 'Transformer Block',
+      fw: 'pytorch',
+      code: `import torch
+  import torch.nn as nn
+  
+  class TransformerBlock(nn.Module):
+      def __init__(self):
+          super().__init__()
+          self.embed = nn.Embedding(10000, 512)
+          self.attn = nn.MultiheadAttention(embed_dim=512, num_heads=8, dropout=0.1, batch_first=True)
+          self.ln1 = nn.LayerNorm([512])
+          self.fc1 = nn.Linear(512, 2048)
+          self.drop = nn.Dropout(p=0.1)
+          self.fc2 = nn.Linear(2048, 512)
+          self.ln2 = nn.LayerNorm([512])
+  
+      def forward(self, x):
+          x = self.embed(x)
+          x, _ = self.attn(x, x, x)
+          x = self.ln1(x)
+          x = self.fc1(x)
+          x = self.drop(x)
+          x = self.fc2(x)
+          x = self.ln2(x)
+          return x`,
+    },
+    {
+      label: 'Keras Sequential',
+      fw: 'tensorflow',
+      code: `import tensorflow as tf
+  from tensorflow.keras import layers, Model
+  
+  inputs = tf.keras.Input(shape=(224, 224, 3))
+  x1 = layers.Conv2D(32, kernel_size=3, strides=1, padding='same', activation='relu')(inputs)
+  p1 = layers.MaxPooling2D(pool_size=2, strides=2)(x1)
+  x2 = layers.Conv2D(64, kernel_size=3, padding='same', activation='relu')(p1)
+  p2 = layers.MaxPooling2D(pool_size=2, strides=2)(x2)
+  bn1 = layers.BatchNormalization()(p2)
+  flat = layers.Flatten()(bn1)
+  d1 = layers.Dense(256, activation='relu')(flat)
+  drop1 = layers.Dropout(0.5)(d1)
+  d2 = layers.Dense(10)(drop1)
+  
+  model = Model(inputs=inputs, outputs=d2)`,
+    },
+  ]
 export default function CodeImportPanel() {
   const showCodeImport   = useGraphStore(s => s.showCodeImport)
   const closeCodeImport  = useGraphStore(s => s.closeCodeImport)
